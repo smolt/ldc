40a2e7c64b83ae83fcb0dc3496d66fed2da119a4
Dan Olson <danole@comcast.net>
2015-01-15 07:54:41 -0800

    Fix -vtls help string

--- driver/cl_options.cpp
+++ driver/cl_options.cpp
@@ -66,7 +66,7 @@ static cl::opt<bool, true> verbose("v",
     cl::location(global.params.verbose));
 
 static cl::opt<bool, true> verboseTls("vtls",
-    cl::desc("list TLS variables (useful if -fthread-model=none)"),
+    cl::desc("list TLS variables (useful if -disable-tls used)"),
     cl::ZeroOrMore,
     cl::location(global.params.vtls));
 
