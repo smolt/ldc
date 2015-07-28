e6a98c5a8fc0be788090693bec1ee1e99d2860e8
Dan Olson <danole@comcast.net>
2015-07-27 22:15:55 -0700

    iOS ARM - Follow ABI for integer-like struct return
    For better C compatibility, return "simple" integer-like structs
    in r0 instead of an sret arg per the APCS 10.3.3.

--- gen/abi-ios.cpp
+++ gen/abi-ios.cpp
@@ -7,9 +7,66 @@
 //
 //===----------------------------------------------------------------------===//
 
+/*
+  The iOS ARM ABI is based on a variant of the older APCS:
+
+  http://infocenter.arm.com/help/topic/com.arm.doc.dui0041c/DUI0041C.pdf
+
+  It is highly confusing because the iOS docummentation explicitly refers to
+  the AAPCS in armv6 section, but clang source and LLVM mail lists says
+  otherwise.
+
+  https://developer.apple.com/library/ios/documentation/Xcode/Conceptual/iPhoneOSABIReference/Introduction/Introduction.html
+
+*/
+
 #include "gen/abi.h"
 #include "gen/abi-generic.h"
 #include "gen/abi-ios.h"
+
+// local stuff
+namespace {
+
+bool isStructIntegerLike(TypeStruct* t)
+{
+    // To be integer-like, all fields must be addressed at offset 0
+    // (e.g. union or bit-fields) and must be integral type, pointer (we
+    // extend to D pointer-ish types like class ref or AA), or another
+    // integer-like struct.  clang's isIntegerLikeType() in TargetInfo.cpp
+    // does something similar.
+    //
+    // note: no need to check size or isPOD again because those can't
+    // change.
+
+    VarDeclarations fields = t->sym->fields;
+    for (size_t i = 0; i < fields.dim; ++i)
+    {
+        if (fields[i]->offset != 0)
+            return false;
+
+        Type* ft = fields[i]->type;
+        if (!(ft->isintegral() || ft->ty == Tpointer ||
+              ft->ty == Tclass || ft->ty == Taarray ||
+              (ft->ty == Tstruct && isStructIntegerLike((TypeStruct*)ft))))
+            return false;
+    }
+
+    return true;
+}
+
+bool isStructSimple(TypeStruct* t)
+{
+    // Is this struct simple?  From APCS 10.3.3 "a structure is considered
+    // integer-like if its size is less than or equal to one word, and the
+    // offset of each of its addressable subfields is zero. An integer-like
+    // structured result is considered simple and is returned in register a1
+    // [that is r0]."  This should only applies to D "POD" structs (C
+    // compatible).
+    return (t->Type::size() <= 4 &&
+            t->sym->isPOD() &&
+            isStructIntegerLike(t));
+}
+} // end local stuff
 
 struct IOSTargetABI : TargetABI
 {
@@ -25,8 +82,6 @@ struct IOSTargetABI : TargetABI
         case LINKd:
         case LINKdefault:
             return llvm::CallingConv::Fast;
-            // TODO: if we want to match the iOS C abi for D calls.
-            //return llvm::CallingConv::C;
         default:
             llvm_unreachable("Unhandled D linkage type.");
         }
@@ -34,18 +89,16 @@ struct IOSTargetABI : TargetABI
 
     bool returnInArg(TypeFunction* tf)
     {
-        // TODO: this needs to be fixed to return in r0 anything that fits in
-        // 32-bits 
         if (tf->isref)
             return false;
 
-        // Return structs and static arrays on the stack. The latter is needed
-        // because otherwise LLVM tries to actually return the array in a number
-        // of physical registers, which leads, depending on the target, to
-        // either horrendous codegen or backend crashes.
+        // Normally return static arrays and structs in an sret arg, but need
+        // to make an exception for "simple" integer-like structs to be
+        // compatible with the C ABI.  APCS 10.3.3 says integer-like structs
+        // should be returned in r0.
         Type* rt = tf->next->toBasetype();
-        return (rt->ty == Tstruct || rt->ty == Tsarray);
-        //return false;
+        return ((rt->ty == Tstruct && !isStructSimple((TypeStruct*)rt)) ||
+                rt->ty == Tsarray);
     }
 
     bool passByVal(Type* t)
